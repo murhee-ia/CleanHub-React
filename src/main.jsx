a1d7572b5f66d728eb8966839f658a8a092bbe90
Cristina <mcrispasague@gmail.com>
2025-03-16 14:44:37 +0800

    Preparing index.html, router, ContextProvider, and axios-client for http api requests

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { ContextProvider } from './context/ContextProvider.jsx'
+import router from "./router.jsx"
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ContextProvider>
+      <RouterProvider router={router} />
+    </ContextProvider>
   </StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // Listen on all available network interfaces
+    port: 5173,  // Default Vite port
+    cors: true   // Enable CORS for cross-origin requests
+  }
 })
