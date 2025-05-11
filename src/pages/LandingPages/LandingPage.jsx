5733a156efb1a22410ef6084e0f24401e40a9e4f
Cristina <mcrispasague@gmail.com>
2025-05-11 17:03:41 +0800

    Adds LandingPage.jsx

---
+++ src/pages/LandingPages/LandingPage.jsx
@@ -0,0 +1,18 @@
+const LandingPage = () => {
+
+  const styles = {
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'relative',
+    zIndex: 0,
+    color: '#ffffff'
+  }
+
+  return (
+    <div className="bg-gradient-green" style={styles}>
+    </div>
+  )
+}
+
+export default LandingPage
