a325859f432eec56b67e25a7a0b1de39be4d94a0
Cristina <mcrispasague@gmail.com>
2025-05-20 23:19:22 +0800

    Creates About page

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import LandingPage from './pages/LandingPages/LandingPage'
+import AboutPage from "./pages/LandingPages/AboutPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <LandingPage />
+    },
+    {
+        path: '/l-about',
+        element: <AboutPage/>
     }
 ]);
 
